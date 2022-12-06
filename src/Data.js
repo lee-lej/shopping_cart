73aa294dd1f0d5c625f7ec16facdc8940588a06a
enkelejdadomi <kela.domi@gmail.com>
2022-12-06 18:26:28 -0500

    first commit

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "snb",
+    name: "SnowBoard",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "bnd",
+    name: "Bindings",
+    price: 150,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "bts",
+    name: "Boots",
+    price: 200,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "ggl",
+    name: "Goggles",
+    price: 70,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+];
